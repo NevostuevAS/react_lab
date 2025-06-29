fc5b73b16340c9b5eb1e626b8ee3b43410281531
NevostruevAS <you@example.com>
2025-06-29 20:50:33 +0700

    1_1_2 Fix the return statement

--- src/1/1_1_2/App.tsx
+++ src/1/1_1_2/App.tsx
@@ -4,8 +4,7 @@
 */
 
 export default function Profile() {
-    return
-      <img src="KatsukoSaruhashi.jpg" alt="Katsuko Saruhashi" />;
+    return <img src="KatsukoSaruhashi.jpg" alt="Katsuko Saruhashi" />;
   }
   
   

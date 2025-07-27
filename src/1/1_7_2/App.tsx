2fa8a97e2180c17b480b4da5797e305ec0bc5334
NevostruevAS <you@example.com>
2025-07-27 19:37:28 +0700

    1_7_2 Nested lists in one component

--- src/1/1_7_2/App.tsx
+++ src/1/1_7_2/App.tsx
@@ -1,14 +1,33 @@
-// 1_7_2 Nested lists in one component 
+// 1_7_2 Nested lists in one component
 /*
   Создайте список рецептов из этого массива! Для каждого рецепта в массиве выведите его название в виде <h2> и список ингредиентов в виде <ul>. Это потребует вложения двух различных вызовов map.
 */
 
 import { recipes } from './data';
 
+export type Recipe = {
+    id: string;
+    name: string;
+    ingredients: string[];
+}
+
 export default function RecipeList() {
+    const reclist = recipes.map((recipe) => (
+        <li key={recipe.id}>
+            <h2>{recipe.name}</h2>
+            <p>ingredients:</p>
+            <ul>
+                {recipe.ingredients.map((ingredient, index) => (
+                    <li key={index}>{ingredient}</li>
+                ))}
+            </ul>
+        </li>
+    ));
+
     return (
         <div>
             <h1>Recipes</h1>
+            <ul>{reclist}</ul>
         </div>
     );
 }

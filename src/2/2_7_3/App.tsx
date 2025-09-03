af8bf240b191f40a2942bf19302fb4a57290fb37
NevostruevAS <doublemouthfuls@gmail.com>
2025-09-03 20:34:48 +0700

    2_7_3 Fix the mutations using non-mutative methods

--- src/2/2_7_3/App.tsx
+++ src/2/2_7_3/App.tsx
@@ -24,26 +24,24 @@ export default function TaskApp() {
     const [todos, setTodos] = useState(initialTodos);
 
     function handleAddTodo(title: string) {
-        todos.push({
+        setTodos([
+        ...todos,
+        {
             id: nextId++,
-            title: title,
+            title,
             done: false,
-        });
+        }
+    ]);
     }
 
     function handleChangeTodo(nextTodo: Todo) {
-        const todo = todos.find(
-            (t) => t.id === nextTodo.id
-        )!!;
-        todo.title = nextTodo.title;
-        todo.done = nextTodo.done;
+        setTodos(todos.map(todo => 
+        todo.id === nextTodo.id ? { ...todo, ...nextTodo } : todo));
     }
 
     function handleDeleteTodo(todoId: number) {
-        const index = todos.findIndex(
-            (t) => t.id === todoId
-        );
-        todos.splice(index, 1);
+          setTodos(todos.filter(todo => todo.id !== todoId));
+
     }
 
     return (

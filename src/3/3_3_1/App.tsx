f4f8a9bfc8ac35d5e5a0c3ebb2e8d6edfc0f284f
NevostruevAS <doublemouthfuls@gmail.com>
2025-09-04 19:55:02 +0700

    3_3_1 Synced inputs

--- src/3/3_3_1/App.tsx
+++ src/3/3_3_1/App.tsx
@@ -6,30 +6,25 @@
 import { useState } from 'react';
 
 export default function SyncedInputs() {
-  return (
-    <>
-      <Input label="First input" />
-      <Input label="Second input" />
-    </>
-  );
-}
-
-function Input({ label }: { label: string }) {
   const [text, setText] = useState('');
 
-  function handleChange(e) {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setText(e.target.value);
   }
 
   return (
-    <label>
-      {label}
-      {' '}
-      <input
-        value={text}
-        onChange={handleChange}
-      />
-    </label>
+    <>
+      <Input label="First input" value={text} onChange={handleChange} />
+      <Input label="Second input" value={text} onChange={handleChange} />
+    </>
   );
 }
 
+function Input({ label, value, onChange }: { label: string; value: string; onChange: (e: React.ChangeEvent<HTMLInputElement>) => void }) {
+  return (
+    <label>
+      {label}{' '}
+      <input value={value} onChange={onChange} />
+    </label>
+  );
+}

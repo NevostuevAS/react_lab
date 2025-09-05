12e571bd5f4c6edc927a6f27f5f78d90af180bda
NevostruevAS <doublemouthfuls@gmail.com>
2025-09-06 03:22:02 +0700

    3_5_4  Implement useReducer from scratch

--- src/3/3_5_4/MyReact.ts
+++ src/3/3_5_4/MyReact.ts
@@ -3,10 +3,13 @@ import { useState } from 'react';
 export function useReducer<State, Action>(
     reducer: (state: State, action: Action) => State,
     initialState: State
-) {
+): [State, (action: Action) => void] {
   const [state, setState] = useState(initialState);
 
-  // ???
+  function dispatch(action: Action) {
+    const newState = reducer(state, action);
+    setState(newState);
+  }
 
   return [state, dispatch];
 }

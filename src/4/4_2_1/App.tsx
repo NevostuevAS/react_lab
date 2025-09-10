2fff2cf5ca1a97163f694fa1490a2f8d05177688
NevostuevAS <doublemouthfuls@gmail.com>
2025-09-10 19:22:32 +0700

    4_2_1 Play and pause the video

--- src/4/4_2_1/App.tsx
+++ src/4/4_2_1/App.tsx
@@ -9,23 +9,41 @@ import { useState, useRef } from 'react';
 
 export default function VideoPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
+  
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   function handleClick() {
     const nextIsPlaying = !isPlaying;
     setIsPlaying(nextIsPlaying);
+
+    if (!videoRef.current) return;
+
+    if (nextIsPlaying) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
   }
 
   return (
     <>
+
       <button onClick={handleClick}>
         {isPlaying ? 'Pause' : 'Play'}
       </button>
-      <video width="250">
+
+    
+      <video
+        width="250"                   
+        ref={videoRef}                
+        onPlay={() => setIsPlaying(true)}
+        onPause={() => setIsPlaying(false)}
+      >
         <source
           src="flower.mp4"
           type="video/mp4"
         />
       </video>
     </>
-  )
+  );
 }

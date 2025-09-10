e1ce2b0fb2f8e499aaf0efbecfe43f0f1be35cbe
NevostuevAS <doublemouthfuls@gmail.com>
2025-09-10 19:27:14 +0700

    4_2_2 Focus the search field

--- src/4/4_2_2/App.tsx
+++ src/4/4_2_2/App.tsx
@@ -3,15 +3,27 @@
   Сделайте так, чтобы нажатие на кнопку "Поиск" наводило фокус на поле.
 */
 
+import { useRef } from 'react';
+
 export default function Page() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function handleFocus() {
+    if (inputRef.current) {
+      inputRef.current.focus(); 
+    }
+  }
+
   return (
     <>
       <nav>
-        <button>Search</button>
+        <button onClick={handleFocus}>Search</button>
       </nav>
       <input
         placeholder="Looking for something?"
+        ref={inputRef}
       />
     </>
   );
 }
+

4d322eaa9dc401f94d023eeb11cf752c427b9619
NevostuevAS <doublemouthfuls@gmail.com>
2025-09-11 17:11:55 +0700

    4_5_2 Switch synchronization on and off

--- src/4/4_5_2/App.tsx
+++ src/4/4_5_2/App.tsx
@@ -11,36 +11,45 @@ export default function App() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [canMove, setCanMove] = useState(true);
 
-  useEffect(() => {
-    function handleMove(e) {
-      setPosition({ x: e.clientX, y: e.clientY });
-    }
-    window.addEventListener('pointermove', handleMove);
-    return () => window.removeEventListener('pointermove', handleMove);
-  }, []);
+	useEffect(() => {
+		function handleMove(e) {
+			if (canMove) {
+				setPosition({ x: e.clientX, y: e.clientY });
+			}
+		}
+		
+		window.addEventListener('pointermove', handleMove);
+		
+		return () => {
+			window.removeEventListener('pointermove', handleMove);
+		};
+	}, [canMove]); 
 
   return (
     <>
       <label>
-        <input type="checkbox"
+        <input
+          type="checkbox"
           checked={canMove}
-          onChange={e => setCanMove(e.target.checked)} 
+          onChange={e => setCanMove(e.target.checked)}
         />
         The dot is allowed to move
       </label>
       <hr />
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'pink',
-        borderRadius: '50%',
-        opacity: 0.6,
-        transform: `translate(${position.x}px, ${position.y}px)`,
-        pointerEvents: 'none',
-        left: -20,
-        top: -20,
-        width: 40,
-        height: 40,
-      }} />
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'pink',
+          borderRadius: '50%',
+          opacity: 0.6,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+          pointerEvents: 'none',
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+        }}
+      />
     </>
   );
 }
